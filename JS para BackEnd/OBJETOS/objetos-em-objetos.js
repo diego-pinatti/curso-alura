36efa654e004becb108daf4771e65606a5bfbd3c
Diego Batista <diego.pinatti87@yahoo.com>
2023-02-22 18:39:18 -0300

    criado diretorio do curso JavaScript BackEnd

---
+++ JS para BackEnd/OBJETOS/objetos-em-objetos.js
@@ -0,0 +1,15 @@
+const cliente = {
+  nome: "João",
+  idade: 24,
+  email: "joao@provedor.com.br",
+  telefone: ["1955555555", "19232332035"]
+};
+
+cliente.endereco = {
+  rua: "R. José da Silva",
+  numero: 789,
+  apartamento: true,
+  complemento: "ap 545",
+}
+
+console.log(cliente)
