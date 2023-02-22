833c5d32d086b37409cfa50847569d7c002598c2
Diego Batista <diego.pinatti87@yahoo.com>
2023-02-22 18:59:55 -0300

    adicionado exercicios de Arrays

---
+++ JS para BackEnd/modulo-ARRAYS/concat.js
@@ -0,0 +1,8 @@
+//Haverá uma palestra sobre padrões de projetos para estudantes das salas de JavaScript e Python. Nossa missão é juntar ambas as salas em uma única lista que exiba o nome de todas as pessoas.
+
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython)
+
+console.log(salasUnificadas)
---
+++ JS para BackEnd/modulo-ARRAYS/map.js
@@ -0,0 +1,12 @@
+let notas = [10, 9.5, 8, 7, 6];
+
+const notasAtualizadas = notas.map((nota) => {
+  return nota + 1 >= 10 ? 10 : nota + 1;
+});
+console.log(notasAtualizadas)
+
+
+// notas.forEach((nota) => {
+//   notas = nota + 1 >= 10 ? 10 : nota + 1;
+//   console.log(notas);
+// })
